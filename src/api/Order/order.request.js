d1f7c98274ccacd61b17a0ce7e80e419de40ee79
Ahamed <fahamed@wiley.com>
2022-11-01 03:05:34 +0530

    🎁order CRUD added

--- src/api/Order/order.request.js
+++ src/api/Order/order.request.js
@@ -9,8 +9,38 @@ const saveOrderRequest = async (order) => {
   }
 };
 
+const getOrdersforManager = async () => {
+  try {
+    const response = await apiInstance.get(`/api/orderNew/manager/`);
+    return response.data;
+  } catch (err) {
+    return err.response;
+  }
+};
+
+const deleteOrderRequest = async (orderID) => {
+  try {
+    const response = await apiInstance.delete(`/api/orderNew/${orderID}`);
+    return response.data;
+  } catch (err) {
+    return err.response;
+  }
+};
+
+const updateOrderRequest = async (order, orderID) => {
+  try {
+    const response = await apiInstance.put(`/api/orderNew/${orderID}`, order);
+    return response.data;
+  } catch (err) {
+    return err.response;
+  }
+};
+
 const orderRequest = {
   saveOrderRequest,
+  getOrdersforManager,
+  updateOrderRequest,
+  deleteOrderRequest,
 };
 
 export default orderRequest;
--- src/pages/PlacedOrderRequest.jsx
+++ src/pages/PlacedOrderRequest.jsx
@@ -5,8 +5,18 @@ import Sidebar from "../components/Sidebar";
 import Cover from "../assets/images/supply.jpg";
 import UpdateOrderRequest from "../components/models/UpdateOrderRequest";
 import Swal from "sweetalert2";
+import orderRequest from "../api/Order/order.request";
 
 const PlacedOrderRequest = () => {
+  const [orders, setorders] = useState([]);
+
+  useEffect(() => {
+    orderRequest.getOrdersforManager().then((res) => {
+      console.log(res.data);
+      setorders(res.data);
+    });
+  }, []);
+
   const deleteOrder = () => {
     Swal.fire({
       title: "Are you sure?",
@@ -43,171 +53,82 @@ const PlacedOrderRequest = () => {
           <div
             class="flex justify-center"
             style={{ marginRight: "40px", display: "grid" }}>
-            <div
-              class="rounded-lg shadow-lg bg-white max-w-m"
-              style={{ height: "170px", width: "800px", marginBottom: "50px" }}>
-              <br />
-
-              <div style={{ display: "flex" }}>
-                <div>
-                  <p style={{ marginLeft: "30px", paddingTop: "30px" }}>
-                    Order ID
-                  </p>
-
-                  <br />
-                </div>
+            {!orders.length ? (
+              <div style={{ width: "1300px" }}>
                 <div
                   style={{
-                    borderLeft: "6px solid black",
-                    height: "140px",
-                    marginLeft: "50px",
-                  }}></div>
+                    background: "lightblue",
+                    marginLeft: "150px",
+                    marginTop: "150px",
+                    paddingLeft: "20px",
+                    paddingRight: "20px",
+                  }}>
+                  <h4>No Order Purchase Created !</h4>
+                  <p>No Orders Were Found.</p>
+                </div>
+              </div>
+            ) : (
+              orders.map((chi) => (
+                <div
+                  class="rounded-lg shadow-lg bg-white max-w-m"
+                  style={{
+                    height: "170px",
+                    width: "800px",
+                    marginBottom: "50px",
+                  }}>
+                  <br />
 
-                <div style={{ display: "flex" }}>
-                  <div style={{ marginLeft: "30px" }}>
-                    <p>Placed By : sample name</p>
-                    <br />
-                    <p>Supplier : sample name</p>
-                    <br />
-                    <p>Order Status : Pending</p>
-                    <br />
-                  </div>
                   <div style={{ display: "flex" }}>
-                    <div style={{ marginLeft: "150px", display: "flex" }}>
-                      <p style={{ paddingRight: "20px" }}>Sample Date</p>
-                      <UpdateOrderRequest />
-                      <div
-                        class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110"
-                        onClick={deleteOrder}>
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor">
-                          <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                          />
-                        </svg>
+                    <div>
+                      <p style={{ marginLeft: "30px", paddingTop: "30px" }}>
+                        Order ID
+                      </p>
+
+                      <br />
+                    </div>
+                    <div
+                      style={{
+                        borderLeft: "6px solid black",
+                        height: "140px",
+                        marginLeft: "50px",
+                      }}></div>
+
+                    <div style={{ display: "flex" }}>
+                      <div style={{ marginLeft: "30px" }}>
+                        <p>Placed By : sample name</p>
+                        <br />
+                        <p>Supplier : sample name</p>
+                        <br />
+                        <p>Order Status : Pending</p>
+                        <br />
+                      </div>
+                      <div style={{ display: "flex" }}>
+                        <div style={{ marginLeft: "150px", display: "flex" }}>
+                          <p style={{ paddingRight: "20px" }}>Sample Date</p>
+                          <UpdateOrderRequest />
+                          <div
+                            class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110"
+                            onClick={deleteOrder}>
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              stroke="currentColor">
+                              <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                              />
+                            </svg>
+                          </div>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            </div>
-            <br />
-
-            <div
-              class="rounded-lg shadow-lg bg-white max-w-m"
-              style={{ height: "170px", width: "800px", marginBottom: "50px" }}>
-              <br />
-
-              <div style={{ display: "flex" }}>
-                <div>
-                  <p style={{ marginLeft: "30px", paddingTop: "30px" }}>
-                    Order ID
-                  </p>
-
-                  <br />
-                </div>
-                <div
-                  style={{
-                    borderLeft: "6px solid black",
-                    height: "140px",
-                    marginLeft: "50px",
-                  }}></div>
-
-                <div style={{ display: "flex" }}>
-                  <div style={{ marginLeft: "30px" }}>
-                    <p>Placed By : sample name</p>
-                    <br />
-                    <p>Supplier : sample name</p>
-                    <br />
-                    <p>Order Status : Pending</p>
-                    <br />
-                  </div>
-                  <div style={{ display: "flex" }}>
-                    <div style={{ marginLeft: "150px", display: "flex" }}>
-                      <p style={{ paddingRight: "20px" }}>Sample Date</p>
-                      <UpdateOrderRequest />
-                      <div
-                        class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110"
-                        onClick={deleteOrder}>
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor">
-                          <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                          />
-                        </svg>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div
-              class="rounded-lg shadow-lg bg-white max-w-m"
-              style={{ height: "170px", width: "800px", marginBottom: "50px" }}>
-              <br />
-
-              <div style={{ display: "flex" }}>
-                <div>
-                  <p style={{ marginLeft: "30px", paddingTop: "30px" }}>
-                    Order ID
-                  </p>
-
-                  <br />
-                </div>
-                <div
-                  style={{
-                    borderLeft: "6px solid black",
-                    height: "140px",
-                    marginLeft: "50px",
-                  }}></div>
-
-                <div style={{ display: "flex" }}>
-                  <div style={{ marginLeft: "30px" }}>
-                    <p>Placed By : sample name</p>
-                    <br />
-                    <p>Supplier : sample name</p>
-                    <br />
-                    <p>Order Status : Pending</p>
-                    <br />
-                  </div>
-                  <div style={{ display: "flex" }}>
-                    <div style={{ marginLeft: "150px", display: "flex" }}>
-                      <p style={{ paddingRight: "20px" }}>Sample Date</p>
-                      <UpdateOrderRequest />
-                      <div
-                        class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110"
-                        onClick={deleteOrder}>
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor">
-                          <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                          />
-                        </svg>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+              ))
+            )}
           </div>
         </div>
       </div>
